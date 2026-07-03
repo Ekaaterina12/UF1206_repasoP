e1148aee59fcc07a21f2e873a2cf6e1eafd791a9
ekaaterina12 <e.goncharova@arefor.com>
2026-07-03 12:43:58 +0200

    final1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import './style.scss'
 
+//“Cógeme todas las puertas”
+document.querySelectorAll('.accordion-collapse').forEach((item) => {
+  item.addEventListener('shown.bs.collapse', function () {
+    setTimeout(() => {
+      this.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+      });
+    }, 2);
+  });
+});
+
+
